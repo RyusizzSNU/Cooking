5e21dc15347013f9c63acddf424ffaa4746689a6
Yoonsung Kim <yskim5892@gmail.com>
2020-11-11 15:03:41 +0900

    wrist pos is now in object coordinate

--- manipulate.py
+++ manipulate.py
@@ -18,18 +18,16 @@ Definitions:
     B : robot base   
 '''
 class Agent():
-    initial_joint = [0.39821290969848633, -4.948345323602194, 1.713571850453512, 4.439863844508789, -2.273259941731588, 2.5900216102600098]
-    initial_joint_90 = [0.398248, -4.94837, 1.71355, 4.43986, -2.27327, 1.01928]
+    initial_pos = [0.4, 0.1, -0.25]
+    initial_rot = [[0, 1, 0], [-math.sqrt(2) / 2, 0, math.sqrt(2) / 2],
+                   [math.sqrt(2) / 2, 0, math.sqrt(2) / 2]]
+    #initial_joint = [0.39821290969848633, -4.948345323602194, 1.713571850453512, 4.439863844508789, -2.273259941731588, 2.5900216102600098]
+    #initial_joint_90 = [0.398248, -4.94837, 1.71355, 4.43986, -2.27327, 1.01928]
     L_W_C = np.array([[-1, 0, 0, 0.05], [0, -1, 0, 0.085], [0, 0, 1, 0.03], [0, 0, 0, 1]])
-    # object center position relative to object pivot(in object coordinate system)
-    v_center_dict = {
-        'carrot' : np.array([0.165, 0, 0, 1]),
-        'pan_handle_handle' : np.array([0, 0.165, -0.02, 1])
-    }
-    # appropriate wrist position relative to object center(in object coordinate system) for gripping
+    # appropriate wrist position relative to object pivot(in object coordinate system) for gripping
     v_wrist_dict = {
-        'carrot' : np.array([0.165, 0, 0, 0]),
-        'pan_handle_handle' : np.array([0, 0.165, 0, 0])
+        'carrot' : np.array([0.17, 0, 0, 1]),
+        'pan_handle_handle' : np.array([0, 0.17, -0.02, 1])
     }
     # appropriate wrist orientation relative to object orientation for gripping
     r_O_W_dict = {
@@ -47,7 +45,8 @@ class Agent():
 
     def ready(self):
         self.gripper.open_gripper()
-        self.robot.movej(self.initial_joint, 0.5, 0.5, relative=False)
+        initial_tcp = np.concatenate([self.initial_pos, R.from_dcm(self.initial_rot).as_rotvec()])
+        self.robot.movel(initial_tcp, 0.5, 0.5, relative=False)
 
     def grip(self, obj):
         L_C_O = utils.dope_to_affine(self.dope_reader.get_obj_pos(obj))
@@ -58,8 +57,8 @@ class Agent():
         target_position = np.matmul(
             L_B_W, np.matmul(
                 self.L_W_C, np.matmul(
-                    L_C_O, self.v_center_dict[obj]
-                ) #+ self.v_wrist_dict[obj]
+                    L_C_O, self.v_wrist_dict[obj]
+                )
             )
         )[:3]
         target_orientation = R.from_dcm(utils.to_33(np.matmul(
@@ -79,5 +78,6 @@ class Agent():
 if __name__ == '__main__':
     agent = Agent()
     agent.ready()
+    time.sleep(1)
     agent.grip('pan_handle_handle')
     agent.robot.close()
